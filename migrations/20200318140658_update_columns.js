fef7cb2a971397cffa0ace60c881146c6671b2ed
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-18 14:32:49 -0600

    updated knex

--- migrations/20200318140658_update_columns.js
+++
@@ -1,7 +0,0 @@
-  exports.up = function (knex) {
-      return knex.schema.renameColumn('cars', tbl => {
-          tbl.string('transmission type', 'transmission-type').index();
-          tbl.string('title status', 'title-status').index();
-      });
-  };
-  exports.down = function (knex) {};
